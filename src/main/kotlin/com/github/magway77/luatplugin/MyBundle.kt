4837a981f90b6116c61693199c8755c66d72fe3c
GitHub Action <action@github.com>
2021-07-07 08:16:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/magway77/luatplugin/MyBundle.kt
+++ src/main/kotlin/com/github/magway77/luatplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.magway77.luatplugin
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/magway77/luatplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/magway77/luatplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.magway77.luatplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.magway77.luatplugin.MyBundle
 
 class MyApplicationService {
 
